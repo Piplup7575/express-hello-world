6b48d03079af10ad229a7386eb45155a4e884012
Piplup <88395302+Piplup7575@users.noreply.github.com>
2023-12-20 18:58:42 -0500

    quick fix

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 /** you can edit the countdown messages below **/
 var countDownDate = new Date("Dec 25, 2023 00:00:00 ").getTime();
-var countdownMessage = "merry chrysler";
+var countdownMessage = "it is chrimis merry chrysler happy crisis";
 /** you can edit the countdown messages above **/
 
 var x = setInterval(function() {
@@ -9,16 +9,13 @@ var x = setInterval(function() {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var seconds = Math.floor((distance % (1000 #* 60)) / 1000);
     document.getElementById('count').innerHTML = "can you believe it guys?! christmas!!! just <u>" + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds +  " seconds</u> away!!! woohoo!!! i am so happy about this information!!! christmas just <u>" + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds +  " seconds</u> away!!! oh wow!!! can you believe it?! christmas!!!";
   
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("happy").innerHTML = countdownMessage;
-        document.getElementById("day").innerHTML = "";
-        document.getElementById("hr").innerHTML = "";
-        document.getElementById("min").innerHTML = "";
-        document.getElementById("sec").innerHTML = "";
+        document.getElementById("count").innerHTML = "";
     } else {
         document.getElementById("happy").innerHTML = "";
     }
