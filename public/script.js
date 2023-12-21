9072be9a51cba100422c6bb9b93bd6999d7a0e01
Piplup <88395302+Piplup7575@users.noreply.github.com>
2023-12-20 19:03:04 -0500

    #

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ var x = setInterval(function() {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 #* 60)) / 1000);
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     document.getElementById('count').innerHTML = "can you believe it guys?! christmas!!! just <u>" + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds +  " seconds</u> away!!! woohoo!!! i am so happy about this information!!! christmas just <u>" + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds +  " seconds</u> away!!! oh wow!!! can you believe it?! christmas!!!";
   
     if (distance < 0) {
